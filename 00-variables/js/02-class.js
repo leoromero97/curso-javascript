85deef8942f19371bb96831e703b4fb2ba2a66c9
leoromero97 <leonardogerbacio@gmail.com>
2023-10-04 17:21:02 -0300

    Clase 02 - Agregue el contenido de la clase 2 de variables - Agregue ejemplos en el archivo 02-class.js

--- 00-variables/js/02-class.js
+++ 00-variables/js/02-class.js
@@ -1,1 +1,19 @@
-/* Existen 3 (tres) formas de declarar una variable */
+/* 
+  Diferencias y ejemplos con la variable de tipo let:
+  - Si se declara una variable con el mismo nombre en diferentes archivos del proyecto, van a ocurrir errores de ejecución.
+  - No se podría utilizar la variable direccion en este archivo, se podría comentar el archivo 01-class.js o usar otros nombres para los ejemlos.
+  - Aplican los mismas características
+  - La diferencia es que, let te permite declarar variables limitando su alcance (scope) al bloque, declaración, o expresión donde se está usando, a diferencia de la palabra reservada var la cual define una variable global o local en una función sin importar el ámbito del bloque.
+*/
+
+// Ejemplos con let:
+
+let nombreCliente = 'Popus';
+console.log(nombreCliente);
+nombreCliente = 'TBBD';
+console.log(nombreCliente);
+nombreCliente = 1;
+console.log(nombreCliente);
+nombreCliente = null;
+console.log(nombreCliente);
+
