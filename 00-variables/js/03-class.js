dbc69d0429dd4f6d4174f7d9723e03e5b6be042a
leoromero97 <leonardogerbacio@gmail.com>
2023-10-04 17:38:13 -0300

    Clase 03: - Agregue el contenido de la clase 3 variables, const - Agregue ejemplos en el archivo 03-class.js

--- 00-variables/js/03-class.js
+++ 00-variables/js/03-class.js
@@ -1,1 +1,24 @@
-/* Existen 3 (tres) formas de declarar una variable */
+/* 
+Diferencias y ejemplos con la variable de tipo const y let:
+- Aplican todas las mismas reglas pero se diferencian en 2 puntos. 
+- Una variable declarada con const no se puede reasignar por otro valor
+- Se debe declarar con un valor si o si.
+
+ */
+
+// Creando una variable y reasignando el valor:
+/*  
+const nombreRepartidor = 'Carlos';
+console.log(nombreRepartidor);  
+nombreRepartidor = 'Juan'; Esto devuelve un error
+console.log(nombreRepartidor);
+*/
+
+// Inicializando una variable sin ningún valor:
+/* const marcaMoto; Esto marca un error
+marcaMoto = 'Honda';
+*/
+
+// Forma correcta:
+const nombreRepartidor = 'Leonardo';
+console.log(nombreRepartidor);
