d22d4b7b5c9d753e436fc8e3839dabb1bc928018
Leo G. Romero <leonardogerbacio@gmail.com>
2024-11-21 15:00:08 -0300

    03 Operators (#4)
    - Modified index.html to variables, strings and numbers
    - Created index.html and js files to operators

---
+++ 03-operators/js/01-operators.js
@@ -0,0 +1,20 @@
+const operatorNumber1 = 20;
+const operatorNumber3 = 30;
+// Operador mayor qué
+console.log(
+  "Comparar si un número es mayor a otro",
+  operatorNumber1 > operatorNumber3
+);
+console.log(
+  "Comparar si un número es mayor a otro, invirtiendo el orden",
+  operatorNumber3 > operatorNumber1
+);
+// Operador menor qué
+console.log(
+  "Comparar si un número es menor a otro",
+  operatorNumber1 < operatorNumber3
+);
+console.log(
+  "Comparar si un número es menor a otro, invirtiendo el orden",
+  operatorNumber3 < operatorNumber1
+);
