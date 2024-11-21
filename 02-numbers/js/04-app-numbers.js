adffa474ebe15ce112824a13dbba835cc63eb6bd
Leo G. Romero <leonardogerbacio@gmail.com>
2024-11-21 14:07:40 -0300

    02 numbers: (#3)
    - Modified index.html in strings folder
    - Created index.html and js files to numbers folders
    - Created index.html main

---
+++ 02-numbers/js/04-app-numbers.js
@@ -0,0 +1,12 @@
+// El orden es igual que en matemáticas, multiplicación y división se ejecutan primero.
+console.log("Primero 30 * 2 y luego suma 20", 20 + 30 * 2);
+
+console.log(
+  "Parentesís para primero que se ejecute la suma y después la multiplicación",
+  (20 + 30) * 2
+);
+
+console.log(
+  "20% de descuento en el carrito de compras:",
+  (20 + 30 + 40 + 50 + 60) * 0.2
+);
---
+++ 02-numbers/js/05-app-numbers.js
@@ -0,0 +1,26 @@
+let puntaje1 = 5;
+console.log(puntaje1);
+// Incrementar
+console.log(
+  "Incrementar de a uno, primero ejecuta el valor inicial y luego incrementa",
+  puntaje1++
+);
+console.log(
+  "Incrementar de a uno, primero incrementa y luego muestra el valor",
+  ++puntaje1
+);
+
+// Decrementar
+console.log(
+  "Decrementar de a uno, primero ejecuta el valor inicial y luego incrementa",
+  puntaje1--
+);
+console.log(
+  "Decrementar de a uno, primero incrementa y luego muestra el valor",
+  --puntaje1
+);
+
+// Incrementar de a más de a uno
+const pointRugby = 3;
+puntaje1 += pointRugby;
+console.log("Se le suma 3 al puntaje inicial", puntaje1);
