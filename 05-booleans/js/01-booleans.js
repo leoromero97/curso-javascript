c1ae244c9b8a05bf8aef5e041957f9accd9f8987
Leo G. Romero <leonardogerbacio@gmail.com>
2024-11-21 16:37:41 -0300

    04-booleans (#5)
    - Create pull_request_template.md file
    - Modified names in folders
    - Created 05-booleans folder with index.html and js files

---
+++ 05-booleans/js/01-booleans.js
@@ -0,0 +1,11 @@
+const boolean1 = true;
+const boolean2 = false;
+console.log(boolean1);
+console.log(boolean2);
+console.log(typeof boolean1);
+
+console.log("Comparar 2 valores booleans:", boolean1 === boolean2);
+
+// Crear un constructor
+const boolean3 = new Boolean(true);
+console.log(boolean3);
